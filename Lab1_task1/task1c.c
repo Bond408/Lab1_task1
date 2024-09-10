5b86af9460fe21419d62367e0573dc389614aa35
flowerb <flowerb@DESKTOP-UQFGV85>
2024-09-11 00:29:28 +0300

    Добавьте файлы проекта.

---
+++ Lab1_task1/task1c.c
@@ -0,0 +1,12 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h> 
+void main()
+{
+	return 0;
+	system("pause");
+	setlocale(LC_ALL, "RUS");
+	printf("Моя программа");
+	getchar();
+
+}
